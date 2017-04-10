52422934ae9e1984ea9b46d533cfec1a4429e4fa
kjlp <kevin.jarnbo@outlook.com>
2017-04-10 11:25:13 +0200

    Lade till felhantering

--- passSec.js
+++ passSec.js
@@ -9,42 +9,51 @@ function register(username, pswd) {
         MongoClient.connect('mongodb://95.85.17.152:27017/test', function(err, db) {
             if (err)
                 throw err;
+
             var collection = db.collection('testeroni');
             var psswdDoc = { 'Username' : username , 'Password' : hash }; 
+            collection.findOne({Username:username}, {Username:1}, function(err, result) {
+                if (err)
+                    throw err;
+                    
+                if (result == null) {
+                    collection.insert(psswdDoc, {w:1}, function(err, result){});
+                    console.log("stored password: " + hash);
+                } else if (result.Username == username) {
+                    console.log("User already exists");
+                } else {
+                    collection.insert(psswdDoc, {w:1}, function(err, result){});
+                    console.log("stored password: " + hash);
+                }
 
-            collection.insert(psswdDoc, {w:1}, function(err, result){});
-            console.log("stored password: " + hash)
+            });     
         });
     });
 }
 
 function retrieve(username, pswd) {
-    var dbHash;
-    bcrypt.hash(pswd,10, function(err, hash){
         MongoClient.connect("mongodb://95.85.17.152:27017/test", function(err, db) {
             if (err)
                 throw err;
+
             var collection = db.collection('testeroni')
             collection.findOne({Username:username}, {Password: 1}, function(err, item){
-                if (bcrypt.compare(item.Password,hash)) //Returnerar true/false
-                {
-                    console.log("correct password");
-                }
-                else {
-                    console.log("incorrect password");
-                }
-                
+                bcrypt.compare(pswd,item.Password, function(err, result) {
+                    if (result == true) //Returnerar true/false
+                    {
+                        console.log("correct password");
+                    }
+                    else {
+                        console.log("incorrect password");
+                    }
+                });
             });
             
         });
-        if (err)
-            throw err;
-            
-        console.log("stored password: " + hash);
-    });
 }
 
-//register("Kjell", password);
 
-retrieve("Kjell", password);
+register("Anna", password);
+
+//retrieve("Anna", password);
 
