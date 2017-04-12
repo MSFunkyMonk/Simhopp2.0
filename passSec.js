24ce5431fb25218c982e1d378d8f4c83f505f1da
kjlp <kevin.jarnbo@outlook.com>
2017-04-12 12:30:35 +0200

    Lade till email i registrerings funktionen

--- passSec.js
+++ passSec.js
@@ -4,26 +4,26 @@ var MongoClient = require('mongodb').MongoClient;
 
 var password = 'Enteredpassword1';
 
-function register(username, pswd) {
+function register(username, pswd, email) {
     bcrypt.hash(pswd,10, function(err, hash){
         MongoClient.connect('mongodb://95.85.17.152:27017/test', function(err, db) {
             if (err)
                 throw err;
 
             var collection = db.collection('testeroni');
-            var psswdDoc = { 'Username' : username , 'Password' : hash }; 
+            var psswdDoc = { 'Username' : username ,'E-mail' : email, 'Password' : hash }; 
             collection.findOne({Username:username}, {Username:1}, function(err, result) {
                 if (err)
                     throw err;
                     
                 if (result == null) {
                     collection.insert(psswdDoc, {w:1}, function(err, result){});
-                    console.log("stored password: " + hash);
+                    console.log("New user created successfully");
                 } else if (result.Username == username) {
                     console.log("User already exists");
                 } else {
                     collection.insert(psswdDoc, {w:1}, function(err, result){});
-                    console.log("stored password: " + hash);
+                    console.log("New user created successfully");
                 }
 
             });     
