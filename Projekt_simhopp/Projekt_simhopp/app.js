966baedff2e7b2b62d2e1ffb638d1d4136cbe931
danne levin <danne4__@hotmail.com>
2017-04-28 11:56:45 +0200

    lagt upp judge mappen med jade filer kommer ändras till html format

--- Projekt_simhopp/Projekt_simhopp/app.js
+++ Projekt_simhopp/Projekt_simhopp/app.js
@@ -41,6 +41,7 @@ app.get('/', function (req, res) {
         res.send(data);
     });
 });
+//hej
 app.get('/*.js', function (req, res) {
     fs.readFile(__dirname + "/public/Admin/" + req.url, 'utf8', function (err, data) {
         res.contentType('javascript');
